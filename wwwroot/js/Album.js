fbdbb2fc203208fb82b1d68f53384df2ac768ebc
RAFAEL ODIARD WESLER <49758734@ort>
2026-07-06 17:20:52 -0300

    landinf

--- wwwroot/js/Album.js
+++ wwwroot/js/Album.js
@@ -1,5 +1,11 @@
+const selecciones = document.querySelectorAll(".seleccion");
+const botonAtras = document.getElementById("anterior");
+const botonAdelante = document.getElementById("siguiente");
+let indiceActual = 0;
 
-
+console.log(document.querySelectorAll(".seleccion"));
+console.log(document.getElementById("anterior"));
+console.log(document.getElementById("siguiente"));
 
 
 async function Pegar(idFigurita, button)
@@ -21,3 +27,28 @@ async function Pegar(idFigurita, button)
 
     }
 }
+function mostrarSelecciones(indice)
+{
+    selecciones[indiceActual].classList.remove("activa");    
+    indiceActual = indice;
+    selecciones[indiceActual].classList.add("activa");
+}
+
+botonAdelante.addEventListener("click", function(e) 
+    {
+        let siguiente = indiceActual + 1;
+        if(siguiente >= selecciones.length)
+        {
+            siguiente = 0;
+        }
+        mostrarSelecciones(siguiente);
+    });
+botonAtras.addEventListener("click", function(e) 
+    {
+        let anterior = indiceActual - 1;
+        if(anterior <= 0)
+        {   
+            anterior = selecciones.length - 1;
+        }
+        mostrarSelecciones(anterior);
+    });
